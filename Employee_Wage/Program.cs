654cc1966d57788a937669977000fe5d388ea0e0
divatepratik10 <pratikrdivate3210@gmail.com>
2022-03-31 21:11:02 +0530

    [Pratik] added:Employee Attendance Programme

--- Employee_Wage/Program.cs
+++ Employee_Wage/Program.cs
@@ -11,6 +11,16 @@ namespace Employee_Wage
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee wage programm.");
+
+            const int EMP_PRESENT = 1; // EMP_ABSENT = 0;
+            Random random = new Random();
+            int empinput = random.Next(0, 2);
+
+            if (empinput == EMP_PRESENT)
+                Console.WriteLine("Employee is Present.");
+            else
+                Console.WriteLine("Employee is absent.");
+
             Console.ReadLine();
         }
     }
